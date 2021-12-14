c9866079d7382d70e631eee3c3e770df81a26a15
Antoni <antoniibrahimdev@gmail.com>
2021-12-14 17:52:24 -0500

    Finished javascript nav bar implementation

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const textileeApp = {};
 
-textileeApp.init = function() {
+textileeApp.navbars = document.querySelector('nav i');
+textileeApp.navUlElement = document.querySelector('nav ul');
+textileeApp.h2Element = document.querySelector('h2');
+
+console.log(textileeApp.navbars);
+console.log(textileeApp.navUlElement);
+console.log(textileeApp.h2Element);
+
+textileeApp.attachEventListener = function (){
     
+    textileeApp.navbars.addEventListener('click', function(e){
+        textileeApp.navbars.classList.toggle('fa-bars');
+        textileeApp.navbars.classList.toggle('fa-times');
+        textileeApp.navbars.classList.toggle("ipositionchanges");
+        textileeApp.navUlElement.classList.toggle("javanav");
+        
+        
+        textileeApp.h2Element.classList.toggle("visuallyhidden");
+        
+    });
+
 }
+
+textileeApp.init = function() {
+    textileeApp.attachEventListener();
+}
+textileeApp.init();
