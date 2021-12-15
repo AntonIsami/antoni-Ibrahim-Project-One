eb7e8b43300b11df28e41f0320990d379baeb5ab
Antoni <antoniibrahimdev@gmail.com>
2021-12-14 20:59:16 -0500

    almost implemented comment capture onto page with live date and time

--- script.js
+++ script.js
@@ -4,26 +4,52 @@ textileeApp.navbars = document.querySelector('nav i');
 textileeApp.navUlElement = document.querySelector('nav ul');
 textileeApp.h2Element = document.querySelector('h2');
 
-console.log(textileeApp.navbars);
-console.log(textileeApp.navUlElement);
-console.log(textileeApp.h2Element);
-
-textileeApp.attachEventListener = function (){
-    
+textileeApp.attachEventListener = function(){
     textileeApp.navbars.addEventListener('click', function(e){
         textileeApp.navbars.classList.toggle('fa-bars');
         textileeApp.navbars.classList.toggle('fa-times');
         textileeApp.navbars.classList.toggle("ipositionchanges");
         textileeApp.navUlElement.classList.toggle("javanav");
-        
-        
         textileeApp.h2Element.classList.toggle("visuallyhidden");
-        
     });
 
+}
+textileeApp.formElement = document.querySelector("form");
+
+
+
+textileeApp.printUserComments = function() {
+    textileeApp.formElement.addEventListener('submit', function(event){
+        event.preventDefault();
+        const userName = document.querySelector(".name").value;
+        const userEmail = document.querySelector("input[type=email]").value;
+        const userMessage = document.querySelector(".message").value;
+        const d = new Date();
+        const commentPost = `
+            <img><i class="fas fa-user-circle"></i></img>
+            <div class="commentText">
+                <p class="date"> 
+                "${d} by ${userName}"
+                </p>
+                <p class="commentp">
+                "${userMessage}"
+                </p>
+            </div>
+        `
+        
+        const newComment = document.createElement('div')
+        newComment.className = "commentAndPicture";
+        newComment.innerHTML= commentPost;
+        
+        const commentsElement = document.getElementsByClassName("comments");
+        commentPost.textContent = newComment;
+        console.log(commentPost);
+
+    })
 }
 
 textileeApp.init = function() {
     textileeApp.attachEventListener();
+    textileeApp.printUserComments();
 }
 textileeApp.init();
